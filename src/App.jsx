654c94d072c22cfcfed03c100c20fb4fbf0d1241
sylla <s67986601@gmail.com>
2023-01-16 10:52:21 +0000

    ADD: the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Containers/Footer";
+import Header from "./Containers/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
