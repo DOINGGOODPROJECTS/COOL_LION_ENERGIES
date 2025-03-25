26e987e7e16e4c3e8a562174e9dad47c41b79d15
Yamoon <160267741+Yamoon224@users.noreply.github.com>
2025-03-25 11:11:04 +0000

    Update Routes.js

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -11,6 +11,7 @@ const Routes = {
   BuildingsRouteLink: "/products/buildings",
   SupportRouteLink: "/products/support",
   ContactRouteLink: "/products/contact",
+  TariffBrochure: "https://coollionenergies.com/Plaquette-tarifs-cool-lion.pdf"
 };
 
 export default Routes;
